75f2ad97f5c5546af16a8d6f3d6b2c39e41f0dda
MadhurTyagi5 <tyagimadhur5425@gmail.com>
2025-09-20 13:34:13 +0530

    Implemented grid system and mouse interactivity with distance based line length

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function(){
     canvas.height = window.innerHeight;
 
        const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+       flowField.animate(0);
     
 }
 
@@ -52,6 +53,43 @@ class FlowFieldEffect {
         this.gradient.addColorStop("0.8", "#80ff80");
         this.gradient.addColorStop("0.9", "#ffff33");
     }
+ animate(timeStamp){
+        let deltaTime = timeStamp - this.lastTime;
+        this.lastTime = timeStamp;
 
+        if (this.timer > this.interval){
+            this.#ctx.clearRect(0,0,this.#width, this.#height);
+            this.#radius += this.vr;
+            if (this.#radius > 5  || this.#radius < -5) this.vr *= -1
+            for (let y = 0; y < this.#height; y+= this.cellSize){
+                for (let x = 0; x < this.#width; x += this.cellSize){
+                    const angle = this.#getValue(x, y);
+                    this.#draw(angle,x,y);
+                }
+            }
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime;
+        }
 
+        requestAnimationFrame(this.animate.bind(this));
+
+    }
+      #getValue(x, y){
+        return (Math.cos(mouse.y/3 * x * 0.00005) + Math.sin(mouse.x/3 * y * 0.00005)) * this.#radius;
+    }
+     #draw(angle,x,y){
+        let positionX = x;
+        let positionY = y;
+        let dx = mouse.x - positionX;
+        let dy = mouse.y - positionY;
+        let distance = (dx * dx + dy * dy);
+        let length = distance > 150000 ? distance : 150000;
+        if (length > 900000) length = 900000;
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(positionX,positionY);
+        this.#ctx.lineTo(positionX+Math.cos(angle) * length/10000, positionY+Math.sin(angle) * length/10000);
+        
+        this.#ctx.stroke();
+    }
 }
