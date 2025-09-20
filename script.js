1822d4077830e33f924ffa2220642008bcca7efa
MadhurTyagi5 <tyagimadhur5425@gmail.com>
2025-09-20 12:16:24 +0530

    Implement FlowField class and core animation loop

--- script.js
+++ script.js
@@ -5,4 +5,42 @@ window.onload = function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+       const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    console.log('loaded');
 }
+
+const mouse = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener('mousemove', function(e){
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+
+
+class FlowFieldEffect {
+    #ctx;
+    #width;
+    #height;
+    #count;
+    #radius;
+    constructor(ctx, width, height, gradient) {
+        this.#ctx = ctx;
+        this.#ctx.lineWidth = 0.3;
+        this.#width = width;
+        this.#height = height;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
+        this.#radius = 1.9;
+        this.vr = 0.03;
+        this.#mapField();
+        this.timer = 0;
+        this.cellSize = 10;
+        this.interval = 1000/60;
+        this.timer = 0;
+        this.lastTime = 0;
+    }
