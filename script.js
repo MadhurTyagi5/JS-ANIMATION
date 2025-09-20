db4c1fa7410b6658636bbb0964df7b733e4172b6
MadhurTyagi5 <tyagimadhur5425@gmail.com>
2025-09-20 12:50:10 +0530

    Added custom color gradient for vector lines

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function(){
     canvas.height = window.innerHeight;
 
        const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-    console.log('loaded');
+    
 }
 
 const mouse = {
@@ -36,11 +36,22 @@ class FlowFieldEffect {
         this.#createGradient();
         this.#ctx.strokeStyle = this.gradient;
         this.#radius = 1.9;
-        this.vr = 0.03;
-        this.#mapField();
+        this.vr = 0.03;       
         this.timer = 0;
         this.cellSize = 10;
         this.interval = 1000/60;
         this.timer = 0;
         this.lastTime = 0;
     }
+    #createGradient(){
+        this.gradient = this.#ctx.createLinearGradient(0, 0,this.#width, this.#height);
+        this.gradient.addColorStop("0.1","#ff5c33");
+        this.gradient.addColorStop("0.2", "#ff66b3");
+        this.gradient.addColorStop("0.4", "#ccccff");
+        this.gradient.addColorStop("0.6", "#b3ffff");
+        this.gradient.addColorStop("0.8", "#80ff80");
+        this.gradient.addColorStop("0.9", "#ffff33");
+    }
+
+
+}
